b0f2cecb4e908e31154ad251e917797085d14dfa
Adnaan <112771255+tareeqadnaan806@users.noreply.github.com>
2023-09-01 17:40:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const value = document.getElementById("inputValue").value
-console.log(value.length)
+const inputvalue = document.getElementById("inputValue");
+
+inputvalue.addEventListener("input", (e) => {
+    const value = inputvalue.value.length;
+    console.log(value);
+    document.getElementById("total").innerText = value
+    document.getElementById("remaining").innerText = -(value-50)
+});
+
